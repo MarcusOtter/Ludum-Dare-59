7fe228433975afa6e4fd42e31d6eb179ee8a961a
Marcus Otterström <35617441+MarcusOtter@users.noreply.github.com>
2026-04-19 04:18:12 +0200

    Add cutout test for result screen

--- Ludum-Dare-59/Assets/Scripts/RenderTextureComprarer.cs
+++ Ludum-Dare-59/Assets/Scripts/RenderTextureComprarer.cs
@@ -1,18 +1,13 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
 
 public class RenderTextureComprarer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMesh;
-
-    [FormerlySerializedAs("renderTextureA")] [SerializeField]
-    private RenderTexture targetRenderTexture;
-
-    [FormerlySerializedAs("renderTextureB")] [SerializeField]
-    private RenderTexture playerRenderTexture;
-
+    [SerializeField] private RenderTexture targetRenderTexture;
+    [SerializeField] private RenderTexture playerRenderTexture;
+    [SerializeField] private SpriteMask testMask;
 
     private void Update()
     {
@@ -28,5 +23,24 @@ public class RenderTextureComprarer : MonoBehaviour
             2f);
         var matchPercent = 100 * score;
         textMesh.text = $"Match: {matchPercent}%";
+
+        var rect = new Rect(0, 0, playerRenderTexture.width, playerRenderTexture.height);
+        var texture2D = ToTexture2D(playerRenderTexture, rect);
+        var sprite = Sprite.Create(texture2D, rect, new Vector2(0.5f, 0.5f));
+
+        testMask.sprite = sprite;
+    }
+
+    // Source - https://stackoverflow.com/a/44265122
+    // Posted by Programmer, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-04-19, License - CC BY-SA 4.0
+    private Texture2D ToTexture2D(RenderTexture rTex, Rect rect)
+    {
+        var tex = new Texture2D(512, 512, TextureFormat.Alpha8, false);
+        // ReadPixels looks at the active RenderTexture.
+        RenderTexture.active = rTex;
+        tex.ReadPixels(rect, 0, 0);
+        tex.Apply();
+        return tex;
     }
 }
